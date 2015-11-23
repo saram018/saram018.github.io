317764a736032f76a0ce223f4505df4d75c5cd33
saram018 <saramer018@gmail.com>
2015-11-23 15:59:36 +0200

    json

---
+++ assg4.js
@@ -0,0 +1,27 @@
+// Your JavaScript goes here...
+
+function parse(){
+	
+	var xmlhttp = new XMLHttpRequest();
+	var url = " http://vlsi.byblos.lau.edu.lb/data.json";
+    xmlhttp.open("GET", url, true);
+
+	xmlhttp.onreadystatechange = function stateChanged() {
+
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var quotes = JSON.parse(xmlhttp.responseText);
+        displayQuotes(quotes);
+		}
+	};
+
+xmlhttp.send();
+}
+
+function displayQuotes(quotes) {
+    var output = "";
+    var i;
+    for(i = 0; i < quotes.length; i++) {
+      output += '<div class="quote"><p class="message">' + quotes[i].content +'</p><p class ="author">' + quotes[i].author + '</p></div>';
+    }
+   document.getElementById("quotes").innerHTML = output;
+}
